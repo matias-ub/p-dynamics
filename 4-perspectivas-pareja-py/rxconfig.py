26d4ccd33f24dd1496ffef5e910f75443d9d62db
Matias Ezequiel Uncal Bressi <matias@offerfit.ai>
2026-02-11 17:05:24 -0300

    remove old

--- 4-perspectivas-pareja-py/rxconfig.py
+++
@@ -1,6 +0,0 @@
-import reflex as rx
-
-config = rx.Config(
-    app_name="4_perspectivas_pareja_py",
-    db_url="sqlite:///reflex.db",
-)
--- 4-perspectivas-pareja-py/state/__init__.py
+++
@@ -1,6 +0,0 @@
-"""State management initialization."""
-from .auth_state import AuthState
-from .test_state import TestState
-from .scoring import ScoringState
-
-__all__ = ["AuthState", "TestState", "ScoringState"]
